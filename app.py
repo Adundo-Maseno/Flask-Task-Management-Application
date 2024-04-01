3c279fd26beeac370292983fd30fab19a06883b4
Moses Adundo <ochiengmusa1995@gmail.com>
2024-04-01 14:49:54 +0300

    update to the app.py file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from models.user import db,User
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session,url_for
 from flask.config import T
 
 
@@ -30,8 +30,8 @@ def register():
         username = request.form['username']
         email = request.form['regEmail']
         password = request.form['regPassword']
-        confirm_password = request.form['confirmPasword']
-        
+        confirm_password = request.form['confirmPassword']
+
          # Check if passwords match
         if password != confirm_password:
             return 'Passwords do not match!'
@@ -52,7 +52,26 @@ def register():
 
         return 'User registered successfully!'
 
-    return render_template('register.html')
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the user exists in the database
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            # If the user exists and the password is correct, log in the user
+            session['user_id'] = user.id
+            return 'Logged in successfully!'
+        else:
+            # If the user does not exist or the password is incorrect, show an error message
+            return 'Invalid username or password!'
+
+    return render_template('index.html')
 
 
 # Run the app
