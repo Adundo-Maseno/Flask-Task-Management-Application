bec71e87827c9df075aeadeeea290fc0a11f55a2
Moses Adundo <ochiengmusa1995@gmail.com>
2024-04-01 21:24:48 +0300

    Day 1: Project Setup and User Authentication and basic user login ui are  done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from models.user import db, User
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,flash
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 
 # Initialize Flask app
@@ -37,23 +37,28 @@ def register():
 
     # Check if passwords match
     if password != confirm_password:
-        return 'Passwords do not match!'
+        flash('Passwords do not match!', 'error')
+        return redirect(url_for('index'))
+        
 
     # Check if username or email already exists in the database
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
-        return 'Username already exists!'
+        flash('Username already exists!', 'error')
+        return redirect(url_for('index'))
 
     existing_email = User.query.filter_by(email=email).first()
     if existing_email:
-        return 'Email already exists!'
+        flash('Email already exists!', 'error')
+        return redirect(url_for('index'))
 
     # Create a new user
     new_user = User(username=username, email=email, password=password)
     db.session.add(new_user)
     db.session.commit()
 
-    return 'User registered successfully!'
+    flash('User registered successfully!', 'success')
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -66,16 +71,19 @@ def login():
     if user and user.check_password(password):
         # If the user exists and the password is correct, log in the user
         login_user(user)
+        flash('Logged in successfully!', 'success')
         return redirect(url_for('index'))
     else:
         # If the user does not exist or the password is incorrect, show an error message
-        return 'Invalid email or password!'
+        flash('Invalid username or password!', 'error')
+        return redirect(url_for('index'))
 
 # Logout route
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
+    flash('Logged out successfully!', 'success')
     return redirect(url_for('index'))
 
 # Run the app
